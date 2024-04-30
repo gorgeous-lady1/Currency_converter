1fafa2edfc49419504e90c417f9f117ad3ef35d5
gorgeous-lady1 <167902564+gorgeous-lady1@users.noreply.github.com>
2024-04-30 16:04:48 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let c_value: {[key: string]: number} ={
+    PKR:278.50,
+    UAE:3.67,
+    USD:1
+
+}
+ let ans = await inquirer.prompt([{
+    type: "list",
+    name:"from",
+    message:"converting from",
+    choices:['PKR','UAE','USD']
+ },
+ {
+    
+       type: "list",
+       name:"to",
+       message:"converting to",
+       choices:['PKR','UAE','USD']
+       },
+    {
+        type:"number",
+        name:"amount",
+        message:"your amount to convert"
+    }
+
+])
+
+
+console.log(c_value[ans.to] / c_value[ans.from] * ans.amount); 
